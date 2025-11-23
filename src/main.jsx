5786f47ed0a6818f6f2433d2f5723422fc6e5ae4
Nishit Bayen <65007263+nishit0072e@users.noreply.github.com>
2025-11-23 18:54:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-.
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FilterLabPro from './FilterLabPro'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FilterLabPro />
+  </React.StrictMode>,
+)
